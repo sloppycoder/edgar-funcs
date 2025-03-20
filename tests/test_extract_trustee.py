73b44630fce6f8f01229f6cd21d599ce9ca33e8b
Li Lin <guru.lin@gmail.com>
2025-03-20 22:09:42 +0800

    added test case for txt filing extraction

--- tests/test_extract_trustee.py
+++ tests/test_extract_trustee.py
@@ -6,7 +6,7 @@ from rag.vectorize.embedding import GEMINI_EMBEDDING_MODEL
 from tests.utils import mock_file_content
 
 
-def test_extract_filing():
+def test_extract_html_filing():
     with patch(
         "rag.extract.trustee.ask_model",
         return_value=mock_file_content(
@@ -26,3 +26,20 @@ def test_extract_filing():
             and result["comp_info"]["trustees"][0]["name"] == "Frank L. Bowman"
             and result["comp_info"]["trustees"][0]["fund_group_compensation"] == "400000"
         )
+
+
+def test_extract_txt_filing():
+    with patch(
+        "rag.extract.trustee.ask_model",
+        return_value=mock_file_content(
+            "response/gemini-1.5-flash-002/39473/0000039473-03-000002.txt"
+        ),
+    ):
+        result = extract_filing(
+            cik="39473",
+            accession_number="0000039473-03-000002",
+            embedding_model=GEMINI_EMBEDDING_MODEL,
+            embedding_dimension=768,
+            model=DEFAULT_LLM_MODEL,
+        )
+        assert result and result["n_trustee"] == 0
