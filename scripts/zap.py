ab2e7094c853c85208de9a3a595100065b2b3d4f
Li Lin <guru.lin@gmail.com>
2025-04-20 22:04:31 +0800

    add utility to delete docs from firestore collection

---
+++ scripts/zap.py
@@ -0,0 +1,30 @@
+import sys
+
+from dotenv import load_dotenv
+from google.cloud import firestore
+
+load_dotenv()
+
+
+def delete_collection(coll_ref, batch_size):
+    docs = coll_ref.limit(batch_size).stream()
+    deleted = 0
+
+    for doc in docs:
+        print(f"Deleting doc {doc.id}")
+        doc.reference.delete()
+        deleted += 1
+
+    if deleted >= batch_size:
+        return delete_collection(coll_ref, batch_size)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python zap.py <collection_name>")
+        sys.exit(1)
+
+    collection_name = sys.argv[1]
+    db = firestore.Client()
+    collection_ref = db.collection(collection_name)
+    delete_collection(collection_ref, 200)
