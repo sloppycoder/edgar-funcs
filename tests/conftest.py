9acc7b6a29513a3351a54ba48d2773de253408e6
Li Lin <guru.lin@gmail.com>
2025-05-03 16:32:15 +0800

    test config update

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ from pathlib import Path
 cwd = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath(f"{cwd}/.."))
 
+# comment the following out to use values set in .env
 os.environ["STORAGE_PREFIX"] = str(Path(__file__).parent / "mockdata/pickle")
-# os.environ["STORAGE_PREFIX"] = "gs://edgar_666/new_funcs"
