27ff501ed0b965a1b5ea30b80608c2573ec63e6d
Li Lin <guru.lin@gmail.com>
2025-05-05 10:19:20 +0800

    cleanup conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,5 @@
 import os
-import sys
 from pathlib import Path
-
-cwd = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.abspath(f"{cwd}/.."))
 
 # comment the following out to use values set in .env
 os.environ["STORAGE_PREFIX"] = str(Path(__file__).parent / "mockdata/pickle")
