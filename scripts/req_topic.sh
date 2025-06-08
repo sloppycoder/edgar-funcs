46fa38ab5e237179fd344e95d64187c77d2ab306
Li Lin <guru.lin@gmail.com>
2025-06-08 18:17:51 +0800

    update script

--- scripts/req_topic.sh
+++ scripts/req_topic.sh
@@ -3,12 +3,13 @@
 PPOJECT_ID=edgar-ai
 REQUEST_TOPIC=edgarai-request
 DLQ_TOPIC=${REQUEST_TOPIC}-dlq
+SUB_EXP=60d
 
 gcloud pubsub topics create "$REQUEST_TOPIC"
-gcloud pubsub subscriptions create $REQUEST_TOPIC-sub --topic $REQUEST_TOPIC
+gcloud pubsub subscriptions create $REQUEST_TOPIC-sub --topic $REQUEST_TOPIC --expiration-period=$SUB_EXP
 
 gcloud pubsub topics create "$DLQ_TOPIC"
-gcloud pubsub subscriptions create $DLQ_TOPIC-sub --topic $DLQ_TOPIC
+gcloud pubsub subscriptions create $DLQ_TOPIC-sub --topic $DLQ_TOPIC --expiration-period=$SUB_EXP
 
 
 gcloud pubsub subscriptions create edgarai-request-trigger \
@@ -21,6 +22,6 @@ gcloud pubsub subscriptions create edgarai-request-trigger \
   --max-delivery-attempts=5 \
   --min-retry-delay=60s \
   --max-retry-delay=600s \
-  --expiration-period=7d
+  --expiration-period=$SUB_EXP
 
 gcloud pubsub subscriptions list
